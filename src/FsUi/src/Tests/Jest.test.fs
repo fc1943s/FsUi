faf061bc6480d1f63b61673bc16d9026abf13837
fc1943s <root@fc1943s.dev>
2021-09-13 01:54:25 -0300

    tests

--- src/FsUi/src/Tests/Jest.test.fs
+++ src/FsUi/src/Tests/Jest.test.fs
@@ -1,9 +1,6 @@
 namespace FsUi.Tests
 
-open FsCore
 open Fable.Jester
-open Fable.Core.JsInterop
-open FsJs
 open FsJs.Bindings
 
 
@@ -11,23 +8,7 @@ module Jest =
     Jest.test (
         "trace log",
         promise {
-            emitJsExpr ("console.log", (emitJsExpr () "console.log")) Jest.jsHookFnBody
-
-            let text = "Jest test"
-            Logger.logTrace (fun () -> text) getLocals
-
-            (Jest.expect ((emitJsExpr () "console.log.mock.calls[0]"): string list))
-                .toEqual (
-                    expect.arrayContaining [
-                        "%c%s%c%s%c%s%c%s"
-                        "color: #EEE"
-                        "[Trace] "
-                        "color: #AAA"
-                        text
-                        "color: #888"
-                        ""
-                    ]
-                )
+            ()
         },
         Jest.maxTimeout
     )
